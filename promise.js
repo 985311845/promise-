e6ce5b30095ac36efccd363c0b8599f7b1feea83
hufeng <985311845@qq.com>
2022-05-28 18:05:57 +0800

    PromiseA+

---
+++ promise.js
@@ -0,0 +1,9 @@
+let promise1 = new Promise((resolve, reject) => { resolve('promise1') });
+let promsie2 = promise1.then(() => {
+    return promsie2
+}, (reason => { return reason }))
+promsie2.then((value) => {
+    console.log(value);
+}, reason => {
+    console.log(reason)
+})
